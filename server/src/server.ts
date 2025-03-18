1af4b71a8ccf0bd53c08982993cb8c5528b3d5d8
Edward J. Apostol <eapostol@users.noreply.github.com>
2025-03-18 11:45:45 -0400

    fix: added updated support for __filename and __dirname with ESM

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 import db from './config/connection.js';
 import routes from './routes/index.js';
+
+// Define __dirname manually
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 await db();
 
